7e1309f55756041f11ad0abd1d65c8bbe82d2e61
Azat Gilyazov <57267956+AnteeOne@users.noreply.github.com>
2021-01-26 23:24:51 +0300

    Update ConfigRepository.java

--- backend/src/main/java/tech/anteeone/ecobits/configs/ConfigRepository.java
+++ backend/src/main/java/tech/anteeone/ecobits/configs/ConfigRepository.java
@@ -2,10 +2,10 @@ package tech.anteeone.ecobits.configs;
 
 public interface ConfigRepository {
 
-    public static final String DB_URI = "jdbc:postgresql://localhost:5432/postgres";
-    public static final String DB_USER = "anteeone";
-    public static final String DB_PASSWORD = "azat230901";
-    public static final String SECURITY_KEY = "v%2jl8!@$*i$2^h!8mo@ziom2(@ace$-t-3%oo)zj4z66g+oc$";
+    public static final String DB_URI = "";
+    public static final String DB_USER = "";
+    public static final String DB_PASSWORD = "";
+    public static final String SECURITY_KEY = "";
 
 
 
